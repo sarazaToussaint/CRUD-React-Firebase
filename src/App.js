f12429c102aee666feb5737f0c7904ef37bd076c
sarazaToussaint <tmsaraza@gmail.com>
2023-04-13 22:56:34 +0200

    Add CRUD operation for the app

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { db } from './firebase-config';
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore';
+
+function App() {
+  const [ newName, setNewName ] = useState("");
+  const [ newAge, setNewAge ] = useState(0);
+
+  const [ users, setUsers ] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, {name: newName, age: Number(newAge)} )
+  }
+
+  const updateUser = async (id, age) => {
+     const userDoc = doc(db, "users", id)
+     const newFileds = {age: age + 1};
+     await updateDoc(userDoc, newFileds)
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  }
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
+    }
+
+    getUsers();
+
+  }, [])
+
+  return (
+    <div className="App">
+      <input 
+       placeholder="Name.." 
+       onChange={(event) => {
+       setNewName(event.target.value);
+      }} />
+      <input type='number' 
+        placeholder="Age.." 
+        onChange={(event) => {
+        setNewAge(event.target.value
+        )
+        }} />
+      
+      <button onClick={createUser}>Create user</button>
+      {users.map((user) => {
+        return (
+          <div>
+            {""}
+            <h3>Name: {user.name}</h3>
+            <h3>Age: {user.age}</h3>
+            <button 
+              onClick={() => {updateUser(user.id, user.age)}}>Increase age</button>
+            <button onClick={() => {deleteUser(user.id);}}>Delete user</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAaUCKExOzZXNKz6eAU3dhhW2XpyING8pY",
+    authDomain: "fir-frontend-671b3.firebaseapp.com",
+    projectId: "fir-frontend-671b3",
+    storageBucket: "fir-frontend-671b3.appspot.com",
+    messagingSenderId: "189678732901",
+    appId: "1:189678732901:web:fc282bd4741959f1a9288c"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+
+  export const db = getFirestore(app);
+  
