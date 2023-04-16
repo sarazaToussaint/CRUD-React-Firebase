3b374b7c4e8675b10a2e17ebf6b3a3818a3efbf4
sarazaToussaint <tmsaraza@gmail.com>
2023-04-16 22:00:28 +0200

    Make changes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,5 +13,5 @@ const firebaseConfig = {
   
   const app = initializeApp(firebaseConfig);
   export const db = getFirestore(app);
-  const auth = getAuth(app);
+  export const auth = getAuth(app);
   
