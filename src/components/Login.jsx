842344c9171456387c6d39b6e198698d8df8d486
sarazaToussaint <tmsaraza@gmail.com>
2023-04-17 07:56:12 +0200

    Create Login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h3>Register User</h3>
+        <input type="email" placeholder='Email...' />
+        <input type="password" placeholder='Password...' />
+
+        <button>Create User</button>
+      </div>
+
+      <div>
+        <h3>Login</h3>
+        <input type="email" placeholder='Email...' />
+        <input type="password" placeholder='Password...' />
+
+        <button>Login</button>
+      </div>
+
+      <h4>User Logged In: </h4>
+
+      <button>Sign Out</button>
+    </>
+  )
+}
+
+export default Login
