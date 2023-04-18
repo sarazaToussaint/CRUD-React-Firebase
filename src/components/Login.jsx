142b8261cdb44b01e9a7f165506c7efa56f90642
sarazaToussaint <tmsaraza@gmail.com>
2023-04-18 22:23:53 +0200

    Remove the blanck space in codes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,7 +14,6 @@ const Login = () => {
     const [loginPassword, setLoginPassword] = useState("");
 
     const [user, setUser] = useState({});
-
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     })
