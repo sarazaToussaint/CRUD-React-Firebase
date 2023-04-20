4ec110ddd44b835923d96d50d53428ae7c3c50ec
sarazaToussaint <tmsaraza@gmail.com>
2023-04-20 22:56:34 +0200

    Add all the route in the App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
 import './App.css';
+import CreatePost from "./Pages/CreatePost";
+import Login from "./Pages/Login";
 
 
 function App() {
@@ -7,7 +10,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={} />
+          <Route path="/" element={<Home />} />
+          <Route path="/createPost" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
