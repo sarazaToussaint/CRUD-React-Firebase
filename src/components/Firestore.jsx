8a4020564e36cc23541c4862f21006ac10125ce2
sarazaToussaint <tmsaraza@gmail.com>
2023-04-20 21:56:57 +0200

    Make some change

--- src/components/Firestore.jsx
+++ src/components/Firestore.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import '../App.css';
 import { db } from '../firebase-config';
 import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore';
 
