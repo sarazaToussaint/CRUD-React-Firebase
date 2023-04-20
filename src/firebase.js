fb684d30e9f87a7eec5fb803e8c3030ccc45e1c9
sarazaToussaint <tmsaraza@gmail.com>
2023-04-20 22:01:20 +0200

    Configure firebase with react

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaUCKExOzZXNKz6eAU3dhhW2XpyING8pY",
+  authDomain: "fir-frontend-671b3.firebaseapp.com",
+  projectId: "fir-frontend-671b3",
+  storageBucket: "fir-frontend-671b3.appspot.com",
+  messagingSenderId: "189678732901",
+  appId: "1:189678732901:web:94df12605b662c3da9288c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
