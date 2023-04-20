6e1da2be0687e90308d542973b2983f702c028af
sarazaToussaint <tmsaraza@gmail.com>
2023-04-20 23:34:55 +0200

    Add the sign in button

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
 
 const Login = () => {
+
+ const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then(() => {
+
+    })
+  }
+
   return (
     <div className="loginPage">
       <p>Sign In With Google to Continue</p>
