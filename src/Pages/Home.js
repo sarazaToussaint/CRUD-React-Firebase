f64d639caacec3d3abb1bcafb7aa5541a9206476
sarazaToussaint <tmsaraza@gmail.com>
2023-04-21 16:03:08 +0200

    Desplay post title and body on the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from "../firebase";
 
 const Home = () => {
+  const [postLists, setPostList] = useState([]);
+
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect (() => {
+   
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    
+    getPosts();
+  }, []);
+
   return (
-    <div className="homePage">Home</div>
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>@{post.auth.name}</h3>
+          </div>
+        );
+      })};
+    </div>
   )
 }
 
