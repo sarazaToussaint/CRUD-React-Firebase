eccf3c8a3bc41fd53131733dacf83f4b29877d69
sarazaToussaint <tmsaraza@gmail.com>
2023-04-21 13:03:57 +0200

    Use useState hook to manage current & update state

--- src/Pages/CreatePost.js
+++ src/Pages/CreatePost.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
   return (
     <div className="createPostPage">
       <div className="cpContainer">
         <h1>Create a Post</h1>
         <div className="inputGp">
           <label>Title:</label>
-          <input type="text" placeholder="Title..." />
+          <input type="text" placeholder="Title..." onChange={(e) => {setTitle(e.target.value)}}/>
         </div>
         <div className="inputGp">
           <label>Post:</label>
-          <textarea placeholder="Post..." />
+          <textarea placeholder="Post..." onChange={(e) => {setPostText(e.target.value)}}/>
         </div>
         <button type="button">Submit Post</button>
       </div>
