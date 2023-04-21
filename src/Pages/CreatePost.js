07654778d6af45f312471d89788874251fc80f13
sarazaToussaint <tmsaraza@gmail.com>
2023-04-21 12:28:53 +0200

    Add create post form

--- src/Pages/CreatePost.js
+++ src/Pages/CreatePost.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const CreatePost = () => {
   return (
-    <div>Post</div>
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create a Post</h1>
+        <div className="inputGp">
+          <label>Title:</label>
+          <input type="text" placeholder="Title..." />
+        </div>
+        <div className="inputGp">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
