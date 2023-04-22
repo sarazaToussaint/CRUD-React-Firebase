4957fad0e0099a4d6387b625c05ce1fb928d4be6
sarazaToussaint <tmsaraza@gmail.com>
2023-04-22 17:26:44 +0200

    Make changes

--- src/firebase.js
+++ src/firebase.js
@@ -12,16 +12,6 @@ const firebaseConfig = {
   appId: "1:784083064360:web:7bc9e75d66d0179dc3c71d"
 };
 
-// const firebaseConfig = {
-//   apiKey: `${process.env.REACT_APP_API_KEY}`,
-//   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-//   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-//   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-//   messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-//   appId: `${process.env.REACT_APP_APP_ID}`
-// };
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
