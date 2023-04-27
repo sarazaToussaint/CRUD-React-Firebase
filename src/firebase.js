3aa8e46f95535614f1e0ba804ed8df572057c1f7
sarazaToussaint <tmsaraza@gmail.com>
2023-04-27 19:55:35 +0200

    Remove a blanck space in firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: "blogdb-a6dbe.firebaseapp.com",
